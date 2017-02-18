063700d76f1773ab329e2eadfee29776bf8eabd4
unknown <>
2017-02-19 06:51:03 +0900

    dfd

---
+++ accounts/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
